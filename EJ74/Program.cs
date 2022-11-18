b2b59151041207cb7e6652851b1bae70d43268a7
Euge <Euge@DESKTOP-S716L4F>
2022-11-17 21:27:16 -0300

    Agregar archivos de proyecto.

---
+++ EJ74/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ74
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Palabra;
+
+            Stack pilaPalabras = new Stack();
+            Console.WriteLine("ingrese la primera palabra a la cola:");
+            Palabra = Console.ReadLine();
+
+            while (Palabra != "")
+            {
+                pilaPalabras.Push(Palabra);
+                Console.WriteLine("Ingrese la próxima palabra");
+                Palabra = Console.ReadLine();
+            }
+            while (pilaPalabras.Count > 0)
+            {
+
+                string texto = (string)pilaPalabras.Pop();
+                Console.WriteLine(texto);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
